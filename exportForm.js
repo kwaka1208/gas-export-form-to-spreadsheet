427e2b329586b18719c3471433fa412444f4ff70
Kenichi Wakabayashi <kenichi@crssrds.jp>
2024-11-17 23:41:06 +0900

    コード追加

---
+++ exportForm.js
@@ -0,0 +1,273 @@
+const panelSheetName = 'パネル'
+
+const COLUMN_NAME = ['No.','グループ名','No.','入力方法','必須','設問内容','説明','No.','選択肢・入力範囲','分岐','検証タイプ','条件','パラメータ1','パラメータ２','表示形式']
+const COLUMN_INDEX = ['GNO','GNAME','QNO','METHOD','REQ','QCONTENT','DESCRIPTION','INO','RANGE','BRANCH','VTYPE','CONDITION','P1','P2','DISPLAY']
+const COLUMN_MAPPING = {
+  QUESTION_CONTENT:   COLUMN_INDEX.indexOf('QCONTENT'),
+  DESCRIPTION:        COLUMN_INDEX.indexOf('DESCRIPTION'),
+  REQUIRED:           COLUMN_INDEX.indexOf('REQ'),
+  INPUT_METHOD:       COLUMN_INDEX.indexOf('METHOD'),
+  RANGE:              COLUMN_INDEX.indexOf('RANGE'),
+  ADDITIONAL_INFO:    COLUMN_INDEX.indexOf('VTYPE'),
+  BRANCH_INFO:        COLUMN_INDEX.indexOf('BRANCH'),
+  VALIDATION_INFO:    COLUMN_INDEX.indexOf('VTYPE'),
+};
+
+function exportForm(formUrl, spreadsheetUrl, sheetName) {
+  const form = FormApp.openByUrl(formUrl);
+  const items = form.getItems();
+
+  // フォームのタイトルと説明を取得
+  const formTitle = form.getTitle();
+  const formDescription = form.getDescription();
+
+  // スプレッドシートを開く
+  let spreadsheet;
+  if (spreadsheetUrl) {
+    try {
+      spreadsheet = SpreadsheetApp.openByUrl(spreadsheetUrl);
+    } catch (e) {
+      // スプレッドシートが見つからない場合、新規作成
+      spreadsheet = SpreadsheetApp.create('Form Questions Output');
+      Logger.log('新しいスプレッドシートが作成されました: ' + spreadsheet.getUrl());
+    }
+  } else {
+    // URLが空の場合、現在開いているスプレッドシートを使用
+    spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  }
+
+  // 指定したシートを取得（存在しない場合は新規作成）
+  let sheet = spreadsheet.getSheetByName(sheetName);
+  if (!sheet) {
+    sheet = spreadsheet.insertSheet(sheetName);
+  } else {
+    // 既存のシートが存在する場合、データを消去（書式などは残す）
+    sheet.clearContents();
+  }
+
+  // ヘッダー行を設定
+  sheet.appendRow(COLUMN_NAME);
+  sheet.appendRow(COLUMN_INDEX);
+
+  // フォームのタイトルと説明をスプレッドシートに追加
+  row = Array(COLUMN_INDEX.length)
+  row[COLUMN_MAPPING.QUESTION_CONTENT] = formTitle;
+  row[COLUMN_MAPPING.INPUT_METHOD] = 'フォームタイトル';
+  sheet.appendRow(row);
+
+  row = Array(COLUMN_INDEX.length);
+  row[COLUMN_MAPPING.QUESTION_CONTENT] = formDescription;
+  row[COLUMN_MAPPING.INPUT_METHOD] = 'フォーム説明';
+  sheet.appendRow(row);
+
+  let currentSectionTitle = '';
+  items.forEach((item) => {
+    let type = item.getType();
+    const title = item.getTitle();
+    const helpText = item.getHelpText();
+    var isRequired = isRequiredForItem(item)
+    let choices = [];
+    let range = '';
+    let inputMethod = '';
+    let columnInfo = [];
+    let rowInfo = [];
+    let additionalInfo = '';
+    let branchInfo = '';
+
+    switch (type) {
+      case FormApp.ItemType.TEXT:
+        inputMethod = 'テキスト';
+        break;
+      case FormApp.ItemType.PARAGRAPH_TEXT:
+        inputMethod = '段落テキスト';
+        break;
+      case FormApp.ItemType.MULTIPLE_CHOICE:
+        inputMethod = 'ラジオボタン';
+        choices = item.asMultipleChoiceItem().getChoices().map(choice => {
+          return choice.getValue();
+        });
+        break;
+      case FormApp.ItemType.CHECKBOX:
+        inputMethod = 'チェックボックス';
+        choices = item.asCheckboxItem().getChoices().map(choice => {
+          return choice.getValue();
+        });
+        break;
+      case FormApp.ItemType.LIST:
+        inputMethod = 'プルダウン';
+        choices = item.asListItem().getChoices().map(choice => choice.getValue());
+        break;
+      case FormApp.ItemType.FILE_UPLOAD:
+        inputMethod = 'ファイルアップロード';
+        break;
+      case FormApp.ItemType.SCALE:
+        inputMethod = '均等目盛';
+        const lowerBound = item.asScaleItem().getLowerBound();
+        const upperBound = item.asScaleItem().getUpperBound();
+        const leftLabel = item.asScaleItem().getLeftLabel(); 
+        const rightLabel = item.asScaleItem().getRightLabel();
+        range = `${leftLabel ? leftLabel : '最小値のラベルなし'}：${lowerBound}, ${rightLabel ? rightLabel : '最大値のラベルなし'}：${upperBound}`;
+        break;
+      case FormApp.ItemType.GRID:
+        inputMethod = '選択式グリッド形式';
+        columnInfo = item.asGridItem().getColumns();
+        rowInfo = item.asGridItem().getRows();
+        break;
+      case FormApp.ItemType.CHECKBOX_GRID:
+        inputMethod = 'チェックボックスグリッド形式';
+        columnInfo = item.asCheckboxGridItem().getColumns();
+        rowInfo = item.asCheckboxGridItem().getRows();
+        break;
+      case FormApp.ItemType.DATE:
+        inputMethod = '日付';
+        break;
+      case FormApp.ItemType.TIME:
+        inputMethod = '時刻';
+        break;
+      case FormApp.ItemType.SECTION_HEADER:
+        inputMethod = 'セクションヘッダー';
+        break;
+      case FormApp.ItemType.PAGE_BREAK:
+        inputMethod = 'セクション';
+        break;
+      case FormApp.ItemType.IMAGE:
+        inputMethod = 'イメージ';
+        additionalInfo = item.getImageAlignment();
+        break;
+      case FormApp.ItemType.VIDEO:
+        inputMethod = 'ビデオ';
+        additionalInfo = item.getVideoTitle();
+        break;
+      case FormApp.ItemType.DATE_TIME:
+        inputMethod = '日付と時刻';
+        break;
+      case FormApp.ItemType.DURATION:
+        inputMethod = '期間';
+        break;
+      default:
+        inputMethod = 'その他';
+    }
+
+    // 設問、説明、必須情報、入力方法、追加情報、分岐情報、検証情報を出力
+    row = Array(COLUMN_INDEX.length);
+    row[COLUMN_MAPPING.QUESTION_CONTENT]  = title;
+    row[COLUMN_MAPPING.DESCRIPTION]       = helpText;
+    row[COLUMN_MAPPING.REQUIRED]          = isRequired;
+    row[COLUMN_MAPPING.INPUT_METHOD]      = inputMethod;
+    row[COLUMN_MAPPING.ADDITIONAL_INFO]   = additionalInfo;
+    row[COLUMN_MAPPING.BRANCH_INFO]       = branchInfo;
+    sheet.appendRow(row);
+
+    row = Array(COLUMN_INDEX.length);
+    if (range != '') {
+        row[COLUMN_MAPPING.RANGE]             = range;
+        sheet.appendRow(row);
+    }
+    // グリッド形式の列情報と行情報を縦方向に追加
+    if (rowInfo.length > 0) {
+      row = Array(COLUMN_INDEX.length);
+      row[COLUMN_MAPPING.RANGE]             = '[項目]';
+      sheet.appendRow(row);
+      rowInfo.forEach((row) => {
+        let gridRow = [];
+        gridRow[COLUMN_MAPPING.RANGE] = row;
+        sheet.appendRow(gridRow);
+      });
+    }
+    if (columnInfo.length > 0) {
+      row = Array(COLUMN_INDEX.length);
+      row[COLUMN_MAPPING.RANGE]             = '[選択肢]';
+      sheet.appendRow(row);
+      columnInfo.forEach((column) => {
+        let gridColumn = [];
+        gridColumn[COLUMN_MAPPING.RANGE] = column;
+        sheet.appendRow(gridColumn);
+      });
+    }
+
+    // 選択肢を縦方向に追加（グリッド形式以外）
+    if (choices.length > 0) {
+      choices.forEach((choice) => {
+        let choiceRow = [];
+        choiceRow[COLUMN_MAPPING.RANGE] = choice;
+        sheet.appendRow(choiceRow);
+      });
+    }
+  });
+
+  var msgComplete = 'フォームの情報がスプレッドシートに出力されました';
+  SpreadsheetApp.getUi().alert(msgComplete);
+  Logger.log(msgComplete + ': ' + spreadsheet.getUrl());
+}
+
+function isRequiredForItem(item) {
+  var result;
+  switch (item.getType()) {
+    case FormApp.ItemType.TEXT:
+      // テキスト
+      result = item.asTextItem().isRequired();
+      break;
+    case FormApp.ItemType.PARAGRAPH_TEXT:
+      // パラグラフテキスト
+      result = item.asParagraphTextItem().isRequired();
+      break;
+    case FormApp.ItemType.MULTIPLE_CHOICE:
+      // ラジオボタン
+      result = item.asMultipleChoiceItem().isRequired();
+      break;
+    case FormApp.ItemType.CHECKBOX:
+      // チェックボックス
+      result = item.asCheckboxItem().isRequired();
+      break;
+    case FormApp.ItemType.LIST:
+      // プルダウン
+      result = item.asListItem().isRequired();
+      break;
+    case FormApp.ItemType.SCALE:
+      result = item.asScaleItem().isRequired();
+      break;
+    case FormApp.ItemType.GRID:
+      result = item.asGridItem().isRequired();
+      break;
+    case FormApp.ItemType.CHECKBOX_GRID:
+      result = item.asCheckboxGridItem().isRequired();
+      break;
+    case FormApp.ItemType.DATE:
+      result = item.asDateItem().isRequired();
+      break;
+    case FormApp.ItemType.TIME:
+      result = item.asTimeItem().isRequired();
+      break;
+    case FormApp.ItemType.DATE_TIME:
+      result = item.asDateTimeItem().isRequired();
+      break;
+    case FormApp.ItemType.SECTION_HEADER:
+      result = false;
+      break;
+    case FormApp.ItemType.DURATION:
+      result = item.asDurationItem().isRequired();
+      break;
+    // 必要に応じて他のタイプも追加可能
+    default:
+      result = item.isRequired ? item.isRequired() : false;
+  }
+  return result ? 'Y' : '';
+}
+
+function exportFromSheetData() {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(panelSheetName);
+  const formUrl = sheet.getRange('C2').getValue();
+  const spreadsheetUrl = sheet.getRange('C3').getValue();
+  const sheetName = sheet.getRange('C4').getValue();
+
+  if (sheetName == panelSheetName) {
+    SpreadsheetApp.getUi().alert('出力先には' + panelSheetName + '以外の名前を使ってください');
+    return;
+  }
+
+  if (formUrl && sheetName) {
+    exportForm(formUrl, spreadsheetUrl, sheetName);
+  } else {
+    SpreadsheetApp.getUi().alert('フォームURLとシート名を正しく入力してください。');
+  }
+}
---
+++ genrateForm.js
@@ -0,0 +1,104 @@
+// GoogleスプレッドシートからGoogleフォームを作成する別のスクリプト
+function generateForm() {
+    // 前回作ったスプレッドシートのURLを指定（必要に応じて変更してください）
+    const sheetUrl = 'YOUR_SPREADSHEET_URL';
+    const spreadsheet = SpreadsheetApp.openByUrl(sheetUrl);
+    const sheetName = 'YOUR_SHEET_NAME';
+    const sheet = spreadsheet.getSheetByName(sheetName);
+  
+    if (!sheet) {
+      Logger.log(`指定されたシート名「${sheetName}」が見つかりません。`);
+      return;
+    }
+  
+    // シートのデータを取得
+    const data = sheet.getDataRange().getValues();
+  
+    // Googleフォームを新規作成
+    const form = FormApp.create('新規生成されたフォーム');
+  
+    // シートのデータを読み込んでフォームに質問を追加
+    for (let i = 2; i < data.length; i++) { // 3行目からスタート
+      const questionText = data[i][3]; // D列に質問内容
+      const inputType = data[i][4];    // E列に入力方法
+      const isRequired = data[i][5] === 'Y'; // F列に必須か任意か（Yの場合は必須）
+      const options = data[i][7] ? data[i][7].split('\n') : []; // H列に選択肢（行区切り）
+      const validationInfo = {          // I, J, K, L 列に回答の検証に関する情報
+        pattern: data[i][8],            // I列に正規表現パターン
+        helpText: data[i][9],           // J列にヘルプテキスト
+        minLength: data[i][10],         // K列に最小文字数
+        maxLength: data[i][11]          // L列に最大文字数
+      };
+  
+      let item;
+      switch (inputType.toLowerCase()) {
+        case 'テキスト':
+          item = form.addTextItem();
+          item.setTitle(questionText);
+          if (validationInfo.pattern) {
+            const textValidation = FormApp.createTextValidation()
+              .requireTextMatchesPattern(validationInfo.pattern)
+              .setHelpText(validationInfo.helpText || '入力が無効です')
+              .build();
+            item.setValidation(textValidation);
+          }
+          if (validationInfo.minLength || validationInfo.maxLength) {
+            const lengthValidation = FormApp.createTextValidation();
+            if (validationInfo.minLength) {
+              lengthValidation.requireTextLengthGreaterThanOrEqualTo(parseInt(validationInfo.minLength));
+            }
+            if (validationInfo.maxLength) {
+              lengthValidation.requireTextLengthLessThanOrEqualTo(parseInt(validationInfo.maxLength));
+            }
+            item.setValidation(lengthValidation.build());
+          }
+          break;
+        case '段落テキスト':
+          item = form.addParagraphTextItem();
+          item.setTitle(questionText);
+          break;
+        case 'チェックボックス':
+          item = form.addCheckboxItem();
+          item.setTitle(questionText);
+          item.setChoices(options.map(option => item.createChoice(option)));
+          break;
+        case 'ラジオボタン':
+          item = form.addMultipleChoiceItem();
+          item.setTitle(questionText);
+          item.setChoices(options.map(option => item.createChoice(option)));
+          break;
+        case 'ドロップダウン':
+          item = form.addListItem();
+          item.setTitle(questionText);
+          item.setChoices(options.map(option => item.createChoice(option)));
+          break;
+        case '日付':
+          item = form.addDateItem();
+          item.setTitle(questionText);
+          break;
+        case '時刻':
+          item = form.addTimeItem();
+          item.setTitle(questionText);
+          break;
+        case '日付時刻':
+          item = form.addDateTimeItem();
+          item.setTitle(questionText);
+          break;
+        case 'セクションヘッダー':
+          item = form.addSectionHeaderItem();
+          item.setTitle(questionText);
+          break;
+        default:
+          Logger.log(`未対応の入力方法: ${inputType}`);
+      }
+  
+      if (item && isRequired && inputType.toLowerCase() !== 'セクションヘッダー') {
+        item.setRequired(true);
+      }
+    }
+  
+    Logger.log('フォームが作成されました: ' + form.getEditUrl());
+  }
+  
+  // 注意: スプレッドシートのURLとシート名を適切に設定してください。
+  
